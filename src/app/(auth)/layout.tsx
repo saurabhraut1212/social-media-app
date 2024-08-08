fc2d5a27abce892602579ac20c373de10004fd7f
Saurabh Raut <saurabhraut3102@gmail.com>
2024-08-08 20:43:47 +0530

    worked on navbar

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+import { validateRequest } from "@/auth";
+import { redirect } from "next/navigation";
+import React from "react";
+
+const LayoutFun = async ({ children }: { children: React.ReactNode }) => {
+  const { user } = await validateRequest();
+
+  if (user) redirect("/");
+  return <>{children} </>;
+};
+
+export default LayoutFun;
---
+++ src/app/(main)/Navbar.tsx
@@ -0,0 +1,20 @@
+import SearchField from "@/components/SearchField";
+import UserButton from "@/components/UserButton";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header className="sticky top-0 z-10 bg-card shadow-sm">
+      <div className="mx-auto flex max-w-7xl flex-wrap items-center gap-5 px-5 py-3">
+        <Link href="/" className="text-2xl font-bold text-primary">
+          BugBook
+        </Link>
+        <SearchField />
+        <UserButton />
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
