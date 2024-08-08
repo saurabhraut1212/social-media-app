52b951c376035b80b2f1ddfe406bd9e64ece784e
Saurabh Raut <saurabhraut3102@gmail.com>
2024-08-08 22:15:16 +0530

    worked on post the posts

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,12 @@
+import PostEditor from "@/components/posts/editor/PostEditor";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="h-[200vh] w-full bg-red-50">
-      <div className="w-full">Front page</div>
+      <div className="w-full">
+        <PostEditor />
+      </div>
     </main>
   );
 }
